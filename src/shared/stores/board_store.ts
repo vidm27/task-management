05e3e48f39f99ed575d6f48ee871d10d52832d8c
vidm27 <davidmoran464@gmail.com>
2024-02-29 06:23:58 -0500

    refact: se limpian los tipos de datos e importaciones

--- src/shared/stores/board_store.ts
+++ src/shared/stores/board_store.ts
@@ -1,21 +1,19 @@
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
+import type { ColumnBoard } from "../types/columnBoard";
+import type { Board } from "../types/board";
 
-interface Board{
-    id: string,
-    nameBoard: string,
-    boardColumn?: string[]
-}
+
 
 export const useBoardStore = defineStore('board', () =>{
     const boards = ref<Board[]>([])
     const totalBoards = computed(() => boards.value.length)
 
-    function addBoard(nameBoard: string, id:string, columns: string[]){
+    function addBoard(nameBoard: string, id:string, columns: ColumnBoard[]){
         boards.value.push({id: id, nameBoard: nameBoard, boardColumn: columns})
     }
 
-    function addColumnBoard(id:string, nameColumns: string[]){
+    function addColumnBoard(id:string, nameColumns: ColumnBoard[]){
         for (const board of boards.value) {
             if(board.id === id){
                 for (const column of nameColumns) {
